d4cbbe70f942e491c475daae54661cc3bec1bd7d
brian gerardo alfonso rodriguez <111832497+Brian1794@users.noreply.github.com>
2024-02-08 08:01:36 -0500

    a
    frontend

---
+++ components/ProductList.js
@@ -0,0 +1,34 @@
+// ProductList.js
+
+import React, { useState, useEffect } from 'react';
+import Servicio from './services/Servicio'; // Importa tu servicio
+
+function ProductList() {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await Servicio.getProductos();
+        setProductos(data);
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Productos</h1>
+      <ul>
+        {productos.map(producto => (
+          <li key={producto.id}>{producto.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
